0de426405e68585161e2fb8d6bbabca82b2ad8d5
haftamu <haftamu7@gmail.com>
2020-10-01 21:02:52 -0500

    firstProgram

---
+++ 01-java-fundamentals/01-first-java-program/FirstProgram.java
@@ -0,0 +1,8 @@
+public class FirstProgram{
+
+public static void main(String[] args) {
+    System.out.println("My name is Haftamu Gabriel \n" + "I am 32 years old \n" + "My home town is Dallas, TX");
+}
+
+
+}
