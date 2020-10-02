1690b61f098ca1b2381c619a2b2355b9807f7ec1
haftamu <haftamu7@gmail.com>
2020-10-01 21:55:38 -0500

    adding pythagorean assignment

---
+++ 01-java-fundamentals/02-pythagorean-theorem/PythagoreanTest.java
@@ -0,0 +1,14 @@
+public class PythagoreanTest{
+    public static void main(String[] args) {
+
+        PythagoreanTheorem hypotenuse = new PythagoreanTheorem();
+         double result = hypotenuse.calculateHypotenuse(4, 9);
+         System.out.println(result);
+
+        
+
+        
+
+
+    }
+}
---
+++ 01-java-fundamentals/02-pythagorean-theorem/PythagoreanTheorem.java
@@ -0,0 +1,13 @@
+
+public class PythagoreanTheorem{
+public double calculateHypotenuse(int legA, int legB){
+
+     double sqr = (legA*legA) + (legB*legB);
+      double hypotenuse = Math.sqrt(sqr);
+
+return hypotenuse;
+
+}
+
+
+}
