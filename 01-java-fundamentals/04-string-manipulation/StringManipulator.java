d983ae8b144555712945f180907335b6092227e8
haftamu <haftamu7@gmail.com>
2020-10-05 11:36:04 -0500

    adding stringManipulation

--- 01-java-fundamentals/04-string-manipulation/StringManipulator.java
+++ 01-java-fundamentals/04-string-manipulation/StringManipulator.java
@@ -4,4 +4,39 @@ public class StringManipulator {
           String newString = first.trim() + second.trim();
            return newString;
     }
+
+    public Integer getIndexOrNull(String name, char value){
+        Integer count =0;
+        for (int i = 0; i < name.length(); i++) {
+            if (name.charAt(i) == value) {
+                count++;
+            } 
+        }
+       return count;
+  }
+
+  public Integer getIndexOrNull(String name, String value){
+      Integer index =  name.indexOf(value);
+      if(value.equals(name.substring(2))){
+        System.out.println(index);
+        
+      }else{
+        System.out.println("null");
+      }
+      return index;
+        
+ }
+   
+ public String concatSubstring(String string1 , int index1, int index2, String string2){
+     String substring = string1.substring(index1, index2);
+     String concatstring = substring + string2;
+     System.out.println(concatstring);
+
+    return concatstring;
+ }
 }
+
+
+
+
+
--- 01-java-fundamentals/04-string-manipulation/StringManipulatorTesting.java
+++ 01-java-fundamentals/04-string-manipulation/StringManipulatorTesting.java
@@ -1,8 +1,40 @@
+
+
 public class StringManipulatorTesting {
     public static void main(String[] args) {
+        //trim a string
         StringManipulator manipulator = new StringManipulator();
-        String str = manipulator.trimAndConcat("    Coding     ","     Dojo    ");
-        System.out.println(str); 
-        
-    }
+         String str = manipulator.trimAndConcat("    Coding     ","     Dojo    ");
+         System.out.println(str); 
+
+
+        //occurence of characters in a string
+        char letter = 'j';
+        String value = "coding dojo";
+        Integer a = manipulator.getIndexOrNull(value, letter);
+            if(a==1){
+                Integer occurrence = value.indexOf(letter);
+                System.out.println(occurrence);
+            }else if(a>1){
+                Integer occurrence2 = value.indexOf(letter,0);
+                System.out.println(occurrence2);
+            }else{
+                System.out.println("character not exist");
+            } 
+    
+    // to find the first index of the substring
+        String word = "Hello";
+        String subString = "llo";
+        String notSubString = "world";
+       manipulator.getIndexOrNull(word, subString);
+       manipulator.getIndexOrNull(word, notSubString);
+
+       //concatenate string and substring
+     manipulator.concatSubstring("Hello", 2, 3, "World");
+
+   
+    
+  
+}
+
 }
