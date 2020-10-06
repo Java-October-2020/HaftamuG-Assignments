c58a834a6bc0c1dd0cf9f3c731db0e6e945352d5
haftamu <haftamu7@gmail.com>
2020-10-05 22:16:16 -0500

    adding string manipulation

--- 01-java-fundamentals/04-string-manipulation/StringManipulator.java
+++ 01-java-fundamentals/04-string-manipulation/StringManipulator.java
@@ -6,31 +6,28 @@ public class StringManipulator {
     }
 
     public Integer getIndexOrNull(String name, char value){
-        Integer count =0;
-        for (int i = 0; i < name.length(); i++) {
-            if (name.charAt(i) == value) {
-                count++;
-            } 
-        }
-       return count;
+       
+            if (name.indexOf(value) !=-1) {
+              return name.indexOf(value);
+            }else{
+              return null;
+            }
+   
+  
   }
 
   public Integer getIndexOrNull(String name, String value){
       Integer index =  name.indexOf(value);
       if(value.equals(name.substring(2))){
-        System.out.println(index);
-        
+        return index;
       }else{
-        System.out.println("null");
+        return null;
       }
-      return index;
-        
  }
    
  public String concatSubstring(String string1 , int index1, int index2, String string2){
      String substring = string1.substring(index1, index2);
      String concatstring = substring + string2;
-     System.out.println(concatstring);
 
     return concatstring;
  }
--- 01-java-fundamentals/04-string-manipulation/StringManipulatorTesting.java
+++ 01-java-fundamentals/04-string-manipulation/StringManipulatorTesting.java
@@ -9,28 +9,27 @@ public class StringManipulatorTesting {
 
 
         //occurence of characters in a string
-        char letter = 'j';
-        String value = "coding dojo";
-        Integer a = manipulator.getIndexOrNull(value, letter);
-            if(a==1){
-                Integer occurrence = value.indexOf(letter);
-                System.out.println(occurrence);
-            }else if(a>1){
-                Integer occurrence2 = value.indexOf(letter,0);
-                System.out.println(occurrence2);
-            }else{
-                System.out.println("character not exist");
-            } 
+        char letter = 'o';
+        Integer a = manipulator.getIndexOrNull("Coding", letter);
+        Integer b = manipulator.getIndexOrNull("Hello World", letter);
+        Integer c = manipulator.getIndexOrNull("Hi", letter);
+        System.out.println(a); 
+        System.out.println(b); 
+        System.out.println(c);  
     
+
     // to find the first index of the substring
         String word = "Hello";
         String subString = "llo";
         String notSubString = "world";
-       manipulator.getIndexOrNull(word, subString);
+        
+       Integer index = manipulator.getIndexOrNull(word, subString);
        manipulator.getIndexOrNull(word, notSubString);
+       System.out.println(index);
 
        //concatenate string and substring
-     manipulator.concatSubstring("Hello", 2, 3, "World");
+        String words = manipulator.concatSubstring("Hello", 1, 2, "world");
+        System.out.println(words); 
 
    
     
