5af852daff9772565aae265b159122bfff440ebd
haftamu <haftamu7@gmail.com>
2020-10-21 09:54:25 -0500

    adding counter

---
+++ 04-java-spring/01-spring-fundamentals/05-counter/counter/src/main/java/com/haftamu/counter/controller/CounterController.java
@@ -0,0 +1,38 @@
+package com.haftamu.counter.controller;
+
+
+import javax.servlet.http.HttpSession;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+
+@Controller
+public class CounterController {
+	
+	@RequestMapping(value="/",method=RequestMethod.GET)
+	public String welcome() {
+		
+
+		return "welcome.jsp";
+	}
+
+	
+	@GetMapping("/counter")
+//	public String counter(@RequestParam(value="counter") String counter, HttpSession session, Model model) {
+	public String counter(HttpSession session, Model model) {
+		Integer currentCount = (Integer) session.getAttribute("counter");
+		if(currentCount == null) {
+	            session.setAttribute("counter", 0);
+	        }
+	        	currentCount++;
+	        	session.setAttribute("counter", currentCount);
+	        	model.addAttribute("counter", currentCount);
+		return "counter.jsp";
+	}
+	
+	
+}
---
+++ 04-java-spring/01-spring-fundamentals/05-counter/counter/src/main/webapp/WEB-INF/counter.jsp
@@ -0,0 +1,24 @@
+<%@ page language="java" contentType="text/html; charset=ISO-8859-1"
+    pageEncoding="ISO-8859-1"%>
+    <%@ taglib prefix = "c" uri = "http://java.sun.com/jsp/jstl/core" %>
+<!DOCTYPE html>
+<html>
+<head>
+<meta charset="ISO-8859-1">
+<title>Counter Page</title>
+<link rel="stylesheet" type="text/css"  href="css/counter.css" >
+</head>
+<body>
+<div class="currentPage">
+
+<p> You have visited  <a href="http://localhost:8080/">http://localhost:8080/</a> ${counter} times</p>
+<a href="http://localhost:8080/">Test another visit?</a>
+
+</div>
+
+
+
+
+
+</body>
+</html>
